5df7e201732cfd141f588afd1691f88081348942
Mohamed Elsharqawy <melsharqawy216@gmail.com>
2026-06-20 05:17:28 +0300

    Add new Updates

---
+++ ImageCanvas.py
@@ -0,0 +1,14 @@
+import customtkinter as ctk
+
+class ImageCanvas(ctk.CTkLabel):
+    def __init__(self, master):
+        pass
+
+    def DisplayImage(self, cv_array):
+        pass
+
+    def ClearCanvas(self):
+        pass
+
+    def ResizeImageToFit(self, pil_image):
+        pass
---
+++ ImageState.py
@@ -0,0 +1,18 @@
+class ImageState:
+    def __init__(self):
+        pass
+
+    def SetInitialImage(self, cv_array, path):
+        pass
+
+    def UpdateImage(self, new_cv_array):
+        pass
+
+    def ResetToOriginal(self):
+        pass
+
+    def GetCurrentImage(self):
+        pass
+
+    def ClearState(self):
+        pass

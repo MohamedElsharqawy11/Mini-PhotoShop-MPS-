fb828bd67f85eda05fca6e2921d66441cbab8d3a
Mohamed Elsharqawy <melsharqawy216@gmail.com>
2026-06-21 20:17:11 +0300

    History_Manager structure with image controller

---
+++ Image_controller.py
@@ -0,0 +1,18 @@
+import cv2
+from customtkinter import filedialog
+
+class ImageController:
+    def __init__(self, main_window, image_state):
+        pass
+
+    def BindEvents(self):
+        pass
+
+    def UploadImage(self):
+        pass
+
+    def SaveImage(self):
+        pass
+
+    def ResetImage(self):
+        pass
---
+++ history_manager.py
@@ -0,0 +1,15 @@
+class HistoryManager:
+    def __init__(self, max_steps=10):
+        pass
+
+    def SaveState(self, current_cv_array):
+        pass
+
+    def Undo(self, current_cv_array):
+        pass
+
+    def Redo(self, current_cv_array):
+        pass
+
+    def ClearHistory(self):
+        pass
